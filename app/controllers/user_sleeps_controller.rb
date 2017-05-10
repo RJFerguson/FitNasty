003462f739632d19bb17230ec8b0d232bde46233
Ryan Ferguson <fergusman@gmail.com>
2017-05-10 13:58:12 -0400

    sleep is rebroken

--- app/controllers/user_sleeps_controller.rb
+++ app/controllers/user_sleeps_controller.rb
@@ -6,21 +6,27 @@ class UserSleepsController < ApplicationController
   end
 
   def create
-    @sleep = UserSleep.new(sleep_params)
+    @user_sleep = UserSleep.new(sleep_params)
 
-    @sleep.user_id = User.find_by(id: session[:user_id]).id
-    @sleep.calories = @sleep.calories_burned
-    if @sleep.save
-      redirect_to user_sleep_path(@sleep)
-    else
+    @user_sleep.user_id = User.find_by(id: session[:user_id]).id
+    @user_sleep.calories = @user_sleep.calories_burned
+    byebug
+    if @user_sleep.save
       redirect_to user_sleep_path
+    else
+      redirect_to new_user_sleep_path
     end
   end
+
+  def show
+    @user = User.find_by(id: session[:user_id]) 
+    @sleep = UserSleep.find(params[:id])
+  end 
 
   private
 
   def sleep_params
-    params.require(:user_sleep).permit(:user_id, :duration, :sleep_date)
+    params.require(:user_sleep).permit(:duration, :sleep_date)
   end
 
 end
