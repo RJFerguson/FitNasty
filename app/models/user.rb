00ea0138afb4f255daa18aed92311115ca045d6e
aisleypay <paisley.singh@gmail.com>
2017-05-10 09:30:40 -0400

    minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,23 +19,23 @@ class User < ApplicationRecord
 
   def total_calories_burned
     exercises = UserExercise.where(user_id: self.id, date_completed: DateTime.parse(Time.now.to_s).strftime("%Y-%m-%d"))
-    sleeping = UserSleep.where(user_id: self.id, sleep_date: DateTime.parse(Time.now.to_s).strftime("%Y-%m-%d"))
+    # sleeping = UserSleep.where(user_id: self.id, sleep_date: DateTime.parse(Time.now.to_s).strftime("%Y-%m-%d"))
 
-    exercises.sum(&:calories) + sleeping.sum(&:calories)
+    exercises.sum(&:calories) #+ sleeping.sum(&:calories)
   end
 
   def net_calories
-    self.total_calories - (self.total_calories_burned)
+    self.total_calories_eaten - (self.total_calories_burned)
   end
 
-  def current_weight
-    curr_weight = self.user_weights.last.weight + (self.net_calories/3500)
-  end
+  # def current_weight
+  #   curr_weight = self.user_weights.last.weight + (self.net_calories/3500)
+  # end
 
   private
 
   #run at end of day
-  
+
   def end_day_weight
     self.user_weights.last.weight + (self.net_calories/3500)
   end
@@ -53,7 +53,8 @@ class User < ApplicationRecord
     day: DateTime.parse(Time.now.to_s).strftime("%Y-%m-%d"),
     daily_weight: self.end_day_weight,
     daily_calories_burned: self.end_day_calories_burned,
-    daily_sleep_duration: UserSleep.where(user_id: self.id, sleep_date: DateTime.parse(Time.now.to_s).strftime("%Y-%m-%d")).sum(&:duration))
+    # daily_sleep_duration: UserSleep.where(user_id: self.id, sleep_date: DateTime.parse(Time.now.to_s).strftime("%Y-%m-%d")).sum(&:duration)
+    )
   end
 
 end
